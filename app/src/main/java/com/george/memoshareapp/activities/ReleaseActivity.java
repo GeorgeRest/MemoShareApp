4977b7e0d37397a2a3efcb00894de84f27ab908f
zxp <1727009010@qq.com>
2023-05-09 15:24:47 +0800

    权限设置+回忆定时

--- app/src/main/java/com/george/memoshareapp/activities/ReleaseActivity.java
+++ app/src/main/java/com/george/memoshareapp/activities/ReleaseActivity.java
@@ -1,17 +1,148 @@
 package com.george.memoshareapp.activities;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.DatePicker;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.george.memoshareapp.R;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class ReleaseActivity extends AppCompatActivity {
+
+    private TextView release_permission;
+    private RelativeLayout rl_permission;
+    private RelativeLayout rl_time;
+    private DateFormat format;
+    private Calendar calendar;
+    private TextView release_time;
+    private TextView release_time_hour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.release);
+        initView();
+        rl_permission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomDialog();
+            }
+        });
+        rl_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(ReleaseActivity.this,  6, release_time, calendar);
+                showTimePickerDialog(ReleaseActivity.this,6,release_time_hour,calendar);
+            }
+        });
 
     }
+
+
+
+    private void initView() {
+        format = DateFormat.getDateTimeInstance();
+        calendar = Calendar.getInstance(Locale.CHINA);
+
+        rl_permission = (RelativeLayout) findViewById(R.id.RL_permission);
+        rl_time = (RelativeLayout) findViewById(R.id.RL_time);
+        release_permission = (TextView) findViewById(R.id.release_permission);
+        release_time = (TextView) findViewById(R.id.release_time);
+        release_time_hour = (TextView) findViewById(R.id.release_time_hour);
+
+    }
+    public static void showDatePickerDialog(Activity activity, int themeResId, final TextView tv, Calendar calendar) {
+        // 直接创建一个DatePickerDialog对话框实例，并将它显示出来
+        new DatePickerDialog(activity, themeResId, new DatePickerDialog.OnDateSetListener() {
+            // 绑定监听器(How the parent is notified that the date is set.)
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                // 此处得到选择的时间，可以进行你想要的操作
+                tv.setText( year + "/" + (monthOfYear + 1) + "/" + dayOfMonth );
+            }
+        }       , calendar.get(Calendar.YEAR)
+                , calendar.get(Calendar.MONTH)
+                , calendar.get(Calendar.DAY_OF_MONTH)).show();
+
+
+
+    }
+    public static void showTimePickerDialog(Activity activity,int themeResId, final TextView tv, Calendar calendar) {
+        // Calendar c = Calendar.getInstance();
+        // 创建一个TimePickerDialog实例，并把它显示出来
+        // 解释一哈，Activity是context的子类
+        new TimePickerDialog( activity,themeResId,
+                // 绑定监听器
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        tv.setText( hourOfDay + ":" + minute  );
+                    }
+                }
+                // 设置初始时间
+                , calendar.get(Calendar.HOUR_OF_DAY)
+                , calendar.get(Calendar.MINUTE)
+                // true表示采用24小时制
+                ,true).show();
+    }
+
+
+    private void showBottomDialog() {
+        //1、使用Dialog、设置style
+        final Dialog dialog = new Dialog(this, R.style.DialogTheme);
+        //2、设置布局
+        View view = View.inflate(this, R.layout.activity_permission, null);
+        dialog.setContentView(view);
+
+        Window window = dialog.getWindow();
+        //设置弹出位置
+        window.setGravity(Gravity.BOTTOM);
+        //设置弹出动画
+        window.setWindowAnimations(R.style.main_menu_animStyle);
+        //设置对话框大小
+        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.show();
+
+        dialog.findViewById(R.id.tv_pc_public).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                release_permission.setText("公开");
+                dialog.dismiss();
+            }
+        });
+
+        dialog.findViewById(R.id.tv_pc_private).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                release_permission.setText("私密");
+                dialog.dismiss();
+            }
+        });
+
+        dialog.findViewById(R.id.tv_cancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+    }
+
+
 }
