c820d2ddd7b0a495cca649c4d7bcd636b10ce057
Seven896 <1525225685@qq.com>
2023-05-15 10:30:16 +0800

    1

---
+++ app/src/main/java/com/george/memoshareapp/activities/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.george.memoshareapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.george.memoshareapp.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
