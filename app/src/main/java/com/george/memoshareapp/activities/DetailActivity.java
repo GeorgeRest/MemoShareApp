21c7dc175799adce37ee98570b56307365815ab6
Seven896 <1525225685@qq.com>
2023-05-23 10:49:33 +0800

    评论7.0

--- app/src/main/java/com/george/memoshareapp/activities/DetailActivity.java
+++ app/src/main/java/com/george/memoshareapp/activities/DetailActivity.java
@@ -30,8 +30,10 @@ import com.george.memoshareapp.beans.CommentBean;
 import com.george.memoshareapp.beans.Post;
 import com.george.memoshareapp.beans.ReplyBean;
 import com.george.memoshareapp.manager.DisplayManager;
+import com.george.memoshareapp.utils.DateFormat;
 import com.george.memoshareapp.view.NoScrollListView;
 
+import org.greenrobot.eventbus.EventBus;
 import org.litepal.LitePal;
 
 import java.util.ArrayList;
@@ -77,6 +79,11 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
     private List<CommentBean> list;
     private ImageView submitComment;           //发送按钮
     private TextView set_comments_number;
+    private boolean has_like;
+    private SharedPreferences sharedPreferences1;
+    private SharedPreferences.Editor editor1;
+    private long likesCount;
+    private String phoneNumber;
 
 
     @Override
@@ -85,7 +92,20 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_detail);
 
         init();
+        sharedPreferences1 = getSharedPreferences("User", MODE_PRIVATE);
+        editor1 = sharedPreferences1.edit();
 
+        likesCount = post.getLike();
+        phoneNumber = sharedPreferences1.getString("phoneNumber", "");
+        has_like = sharedPreferences1.getBoolean(post.getId() + ":" + phoneNumber, false);
+        detail_tv_like_number.setText(String.valueOf(likesCount));
+        if (has_like) {
+            like.setImageResource(R.mipmap.like_press);
+            likesCount++;
+            detail_tv_like_number.setText(String.valueOf(likesCount));
+        } else {
+            like.setImageResource(R.mipmap.like);
+        }
         putParameter2View();//传参
         commentNumber = commentAdapter.getCount();
         detail_tv_share_number.setText(shareNumber+"");
@@ -97,28 +117,24 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
 
     private void putParameter2View() {
 
-//       userIcon.setImageResource((Integer) intent.getExtras().get("userIcon"));
         userIcon.setImageResource(R.mipmap.touxiangceshi);
-        userName.setText(post.getPhoneNumber().substring(0,4));
-        publishTime.setText(post.getPublishedTime());
+        userName.setText(post.getPhoneNumber().substring(0,5));
+        publishTime.setText(DateFormat.getCurrentDateTime(post.getPublishedTime()));
+
         location.setText(post.getLocation());
         content.setText(post.getPublishedText());
         photoPath = post.getPhotoCachePath();
 
+
+
         displayManager.showPhoto(recyclerView,photoPath,DetailActivity.this);
+
 
     }
 
 
     private void init() {
-        sharedPreferences = getSharedPreferences("commentNumberAndLikeNumber", DetailActivity.MODE_PRIVATE);
 
-        shareNumber = sharedPreferences.getInt("shareNumber", 0);
-    //    commentNumber = sharedPreferences.getInt("commentNumber", 0);
-        like_number = sharedPreferences.getInt("likeNumber", 0);
-
-
-        editor = sharedPreferences.edit();
         displayManager = new DisplayManager();
         photoPath = new ArrayList<>();
 
@@ -135,15 +151,11 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
         detail_tv_like_number = (TextView) findViewById(R.id.detail_tv_like_number);
         detail_tv_comment_number = (TextView) findViewById(R.id.detail_tv_comment_number);
         recyclerView = findViewById(R.id.recycler_view);
-        submitComment = (ImageView) findViewById(R.id.submitComment);
-
-
         commentEdit = (EditText) findViewById(R.id.commentEdit);
         commentList = (NoScrollListView) findViewById(R.id.commentList);
         bottomLinear = (LinearLayout) findViewById(R.id.bottomLinear);
         commentLinear = (LinearLayout) findViewById(R.id.commentLinear);
         set_comments_number = (TextView) findViewById(R.id.tv_comments_number);
-
         back.setOnClickListener(this);
         userIcon.setOnClickListener(this);
         share.setOnClickListener(this);
@@ -154,18 +166,6 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
         post = (Post) intent.getSerializableExtra("post");
         commentAdapter = new CommentAdapter(this, getCommentData(),R.layout.comment_item,handler);
         commentList.setAdapter(commentAdapter);
-
-//        commentList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                isReply = true;
-//                postion_reply = position;
-//                commentLinear.setVisibility(View.VISIBLE);
-//                bottomLinear.setVisibility(View.GONE);
-//                onFocusChange(true);
-//            }
-//        });
-
     }
 
     public void onClick(View view) {
@@ -201,27 +201,25 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
                 break;
 
             case R.id.detail_iv_like:
-                if (IS_LIKE ==true){
+                if (has_like) {
                     like.setImageResource(R.mipmap.like);
-                    IS_LIKE = false;
-                    like_number--;
-                    if (like_number>10000){
-                        double converted = Math.floor((double) like_number / 10000 * 10) / 10;
-
-                        detail_tv_like_number.setText(converted+"万");
-                    }
-                    detail_tv_like_number.setText(like_number+"");
-                    break;
+                    likesCount--;
+                    has_like = false;
+                } else {
+                    like.setImageResource(R.mipmap.like_press);
+                    likesCount++;
+                    has_like = true;
                 }
-                like.setImageResource(R.mipmap.like_press);
-                IS_LIKE = true;
-                like_number++;
-                if (like_number>10000){
-                    double converted = Math.floor((double) like_number / 10000 * 10) / 10;
+                editor1.putBoolean(post.getId() + ":" + phoneNumber, has_like);
+                editor1.apply();
+                update();
+                if (likesCount >10000){
+                    double converted = Math.floor((double) likesCount / 10000 * 10) / 10;
 
                     detail_tv_like_number.setText(converted+"万");
+                }else {
+                    detail_tv_like_number.setText(String.valueOf(likesCount));
                 }
-                detail_tv_like_number.setText(like_number+"");
                 break;
             default:
                 break;
@@ -278,14 +276,17 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
 
     }
 
+    private void update() {
+        int totalCount = LitePal.count(Post.class);
+        int id = totalCount - (int) post.getId();
+        EventBus.getDefault().post(new LastClickedPositionEvent(id));
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        editor.putInt("shareNumber",shareNumber);
-    //    editor.putInt("commentNumber",commentNumber);
-        editor.putInt("likeNumber", like_number);
-        editor.apply();
-
+        post.setLike(likesCount);
+        post.update(post.getId());
     }
 
 
@@ -302,88 +303,6 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
             list.add(commentBean);
         }
 
-
-//        CommentBean comment1 = new CommentBean();
-//        comment1.setCommentUserPhoto(R.mipmap.photo_1);
-//        comment1.setCommentUserName("Courtney Henry");
-//        comment1.setCommentTime("11:30");
-//        comment1.setCommentContent("在其中去之前作曲者庆中秋掌权");
-//        comment1.setReplyList(getReplyData());
-//        comment1.setPost(post);
-//        post.getComments().add(comment1);
-//        comment1.save();
-//
-//        post.save();
-//        list.add(comment1);
-//
-////        // 你想要获取的Post对象的ID
-////        long postId =post.getId();
-////
-////        // 加载Post对象及其所有的关联对象
-////        Post post1 = LitePal.find(Post.class, postId, true);
-//
-////        post1.setComments(list);
-//
-//        CommentBean comment2 = new CommentBean();
-//        comment2.setCommentUserPhoto(R.mipmap.photo_2);
-//        comment2.setCommentUserName("Courtney Henry");
-//        comment2.setCommentTime("11:40");
-//        comment2.setCommentContent("在其中去之前作曲者庆中秋掌权在其中去之前作曲者庆中秋掌权 ");
-//        comment2.setReplyList(getReplyData());
-//        comment2.setPost(post);
-//        comment2.save();
-//        list.add(comment2);
-//
-//        CommentBean comment3 = new CommentBean();
-//        comment3.setCommentUserPhoto(R.mipmap.photo_3);
-//        comment3.setCommentUserName("Courtney Henry");
-//        comment3.setCommentTime("12:30");
-//        comment3.setCommentContent("在其中去之前作曲者庆中秋掌权 在其中去之前作曲者庆中秋掌权在其中去之前作曲者庆中秋掌权在其中去之前作曲者庆中秋掌权");
-//        comment3.setReplyList(getReplyData());
-//        comment3.setPost(post);
-//        comment3.save();
-//        list.add(comment3);
-//
-//        CommentBean comment4 = new CommentBean();
-//        comment4.setCommentUserPhoto(R.mipmap.photo_4);
-//        comment4.setCommentUserName("Courtney Henry");
-//        comment4.setCommentTime("14:30");
-//        comment4.setCommentContent("在其中去之前作曲者庆中秋掌权");
-//        comment4.setReplyList(getReplyData());
-//        comment4.setPost(post);
-//        comment4.save();
-//        list.add(comment4);
-//
-//        CommentBean comment5 = new CommentBean();
-//        comment5.setCommentUserPhoto(R.mipmap.photo_5);
-//        comment5.setCommentUserName("Courtney Henry");
-//        comment5.setCommentTime("14:50");
-//        comment5.setCommentContent("在其中去之前作曲者庆中秋掌权在其中去之前作曲者庆中秋掌权");
-//        comment5.setReplyList(getReplyData());
-//        comment5.setPost(post);
-//        comment5.save();
-//        list.add(comment5);
-//
-//
-//        CommentBean comment6 = new CommentBean();
-//        comment6.setCommentUserPhoto(R.mipmap.photo_6);
-//        comment6.setCommentUserName("Courtney Henry");
-//        comment6.setCommentTime("15:30");
-//        comment6.setCommentContent("在其中去之前作曲者庆中秋掌权在其中去之前作曲者庆中秋掌权");
-//        comment6.setReplyList(getReplyData());
-//        comment6.setPost(post);
-//        comment6.save();
-//        list.add(comment6);
-//
-//        CommentBean comment7 = new CommentBean();
-//        comment7.setCommentUserPhoto(R.mipmap.photo_7);
-//        comment7.setCommentUserName("Courtney Henry");
-//        comment7.setCommentTime("17:30");
-//        comment7.setCommentContent("在其中去之前作曲者庆中秋掌权");
-//        comment7.setReplyList(getReplyData());
-//        comment7.setPost(post);
-//        comment7.save();
-//        list.add(comment7);
 
         return list;
     }
@@ -488,31 +407,6 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
     /**
      * 事件点击监听器
      */
-//    private final class ClickListener implements View.OnClickListener {
-//        @Override
-//        public void onClick(View v) {
-//            switch (v.getId()) {
-//                case R.id.submitComment:	//发表评论按钮
-//                    if(isEditEmply()){		//判断用户是否输入内容
-//                        if(isReply){
-//                            replyComment();
-//                        }else{
-//                            publishComment();
-//                        }
-//                        bottomLinear.setVisibility(View.VISIBLE);
-//                        commentLinear.setVisibility(View.GONE);
-//                        onFocusChange(false);
-//                    }
-//                    break;
-//                case R.id.detail_iv_comment:		//底部评论按钮
-//                    isReply = false;
-//                    commentLinear.setVisibility(View.VISIBLE);
-//                    bottomLinear.setVisibility(View.GONE);
-//                    onFocusChange(true);
-//                    break;
-//            }
-//        }
-//    }
 
     @Override
     public void onBackPressed() {
