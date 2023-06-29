d09938cf20853d802e2e656863cca9617822b3a2
zxp <1727009010@qq.com>
2023-06-29 19:53:19 +0800

    zyy

--- app/src/main/java/com/george/memoshareapp/activities/ChangePasswordActivity.java
+++ app/src/main/java/com/george/memoshareapp/activities/ChangePasswordActivity.java
@@ -84,24 +84,9 @@ public class ChangePasswordActivity extends AppCompatActivity implements View.On
                                         } else {
                                             Toasty.error(ChangePasswordActivity.this, "修改失败，请重试", Toast.LENGTH_SHORT,true).show();
                                         }
-//                                        if (response.isSuccessful()) {
-//                                            HttpData<User> apiResponse = response.body();
-//                                            Logger.d(apiResponse.getData());
-//                                            if (apiResponse != null && apiResponse.getCode() == 201) {
-//                                                Toasty.success(ChangePasswordActivity.this, "修改密码成功，请登录", Toast.LENGTH_SHORT).show();
-//                                                finish();
-//                                            } else if (apiResponse != null && apiResponse.getCode() == 200) {
-//                                                Toasty.info(ChangePasswordActivity.this, "该手机号未注册，请先注册", Toast.LENGTH_SHORT, true).show();
-//                                            }
-//                                        }else {
-//                                            Toasty.error(ChangePasswordActivity.this, "修改失败，请重试", Toast.LENGTH_SHORT,true).show();
-//                                        }
                                     }
 
-//                                    @Override
-//                                    public void onFailure(Call<HttpData<User>> call, Throwable t) {
-//                                        Toasty.error(ChangePasswordActivity.this, "网络错误，请重试", Toast.LENGTH_SHORT,true).show();
-//                                    }
+
                                     @Override
                                     public void onFailure(Call<HttpData<User>> call, Throwable t) {
                                         loadingDialog.endAnim(); // 请求成功，结束加载框的动画
@@ -109,8 +94,6 @@ public class ChangePasswordActivity extends AppCompatActivity implements View.On
                                         Logger.d(t.getMessage());
                                     }
                                 });
-
-//                                Toasty.success(ChangePasswordActivity.this, "验证码输入正确", Toast.LENGTH_SHORT, true).show();
 
                             }
                         });
