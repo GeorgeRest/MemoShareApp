ea52113722369602076acaa4e2f798e94b6d40f3
Georege <1561903398@qq.com>
2023-06-29 22:27:45 +0800

    bean

--- app/src/main/java/com/george/memoshareapp/http/response/HttpListData.java
+++ app/src/main/java/com/george/memoshareapp/http/response/HttpListData.java
@@ -2,40 +2,56 @@ package com.george.memoshareapp.http.response;
 
 import java.util.List;
 
-public class HttpListData<T> extends HttpData<HttpListData.ListBean<T>> {
+public class HttpListData<T>  {
+    /** 当前页码 */
+    private int pageIndex;
+    /** 页大小 */
+    private int pageSize;
+    /** 总数量 */
+    private int totalNumber;
+    /** 数据 */
+    private List<T> items;
+    /** 是否为最后一页 */
+    private boolean isLastPage;
 
-    public static class ListBean<T> {
+    public boolean isLastPage() {
+        return isLastPage;
+    }
 
-        /** 当前页码 */
-        private int pageIndex;
-        /** 页大小 */
-        private int pageSize;
-        /** 总数量 */
-        private int totalNumber;
-        /** 数据 */
-        private List<T> items;
+    public void setLastPage(boolean lastPage) {
+        isLastPage = lastPage;
+    }
 
-        /**
-         * 判断是否是最后一页
-         */
-        public boolean isLastPage() {
-            return Math.ceil((float) totalNumber / pageSize) <= pageIndex;
-        }
 
-        public int getTotalNumber() {
-            return totalNumber;
-        }
+    public int getTotalNumber() {
+        return totalNumber;
+    }
 
-        public int getPageIndex() {
-            return pageIndex;
-        }
+    public int getPageIndex() {
+        return pageIndex;
+    }
 
-        public int getPageSize() {
-            return pageSize;
-        }
+    public int getPageSize() {
+        return pageSize;
+    }
 
-        public List<T> getItems() {
-            return items;
-        }
+    public List<T> getItems() {
+        return items;
+    }
+
+    public void setPageIndex(int pageIndex) {
+        this.pageIndex = pageIndex;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public void setTotalNumber(int totalNumber) {
+        this.totalNumber = totalNumber;
+    }
+
+    public void setItems(List<T> items) {
+        this.items = items;
     }
 }
