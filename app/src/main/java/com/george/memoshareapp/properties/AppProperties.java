8d446dfda4fcb5e2fd7fd6ef245104e0edaad3e0
Seven896 <1525225685@qq.com>
2023-07-08 18:31:41 +0800

    配置

--- app/src/main/java/com/george/memoshareapp/properties/AppProperties.java
+++ app/src/main/java/com/george/memoshareapp/properties/AppProperties.java
@@ -5,10 +5,12 @@ public class AppProperties {
     public static final String USER_TABLE_NAME="user";
     public static final String LOCAL_SERVER_SUNNY="http://192.168.43.204:6028";
 
-    public static final String SERVER ="http://192.168.43.128:6028/";
-//    public static final String SERVER="http://82.156.138.114:6027/";
-    public static final String SERVER_MEDIA_URL="http://192.168.43.128/files/media/";
-    public static final String SERVER_RECORD_URL="http://192.168.43.128/files/record/";
+    //public static final String SERVER ="http://192.168.43.128:6028/";
+    //public static final String SERVER_MEDIA_URL="http://192.168.43.128/files/media/";
+    //public static final String SERVER_RECORD_URL="http://192.168.43.128/files/record/";
+    public static final String SERVER="http://82.156.138.114:6027/";
+    public static final String SERVER_MEDIA_URL="http://82.156.138.114/files/media/";
+    public static final String SERVER_RECORD_URL="http://82.156.138.114/files/record/";
     public static final String DEFAULT_AVATAR=SERVER_MEDIA_URL+"default_avatar.png";
 
 }
