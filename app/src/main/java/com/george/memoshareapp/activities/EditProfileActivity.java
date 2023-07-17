6e2e17b7ff63a94b5f2f8883e95466ad3c84d359
Georege <1561903398@qq.com>
2023-07-17 15:10:04 +0800

    commit

--- app/src/main/java/com/george/memoshareapp/activities/EditProfileActivity.java
+++ app/src/main/java/com/george/memoshareapp/activities/EditProfileActivity.java
@@ -68,6 +68,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import es.dmoral.toasty.Toasty;
@@ -222,7 +223,10 @@ public class EditProfileActivity extends AppCompatActivity implements View.OnCli
             tv_edit_gender.setText(gender);
         }
         String birthday = user.getBirthday();
-        if (birthday != null) {
+        if (birthday == null || birthday.isEmpty()) {
+            String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+            tv_edit_birthday.setText(currentDate);
+        } else {
             tv_edit_birthday.setText(birthday);
         }
         String name = user.getName();
